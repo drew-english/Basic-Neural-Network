45d147e0a0f14a7f5c62afaeea5120af81e33e22
Andrew English <aje299@gmail.com>
2018-01-12 16:07:18 -0800

    Update ann.h

--- ann.h
+++ ann.h
@@ -40,15 +40,6 @@ typedef struct ann
 
 typedef double(*Function)(double x);
 
-//memory struct to hold information for experience replay
-typedef struct memory
-{
-	double *oldState;
-	int action;
-	double reward;
-	double *newState;
-} Memory;
-
 //acitvation functions and their derivatives
 double sig(double x);
 double d_sig(double x);
@@ -64,18 +55,9 @@ double *ann_run(const Ann *temp, double *input);
 void ann_fit(Ann *temp, double *input, const double *target);
 void ann_save(Ann *temp, FILE *out);
 Ann *ann_read(FILE *in);
-int q_max(double *output, int numOutputs);
 void ann_fit_batch(Ann *temp, double *inData, double *targetData, int batchSize);
 void reset_cache(Ann *temp);
 double calc_cost(double *output, double *target, int length);
-
-
-//functions specific to this game
-void ann_train(Ann *temp);
-double *normalize_game_board(char *board);
-int *to_coords(const int action);
-void append(double *dest, double *source, int start, int length);
-int test_network(Ann *temp);
 
 //for something to look at while the network is training
 void progress_bar(const int curr, const int total);
